ccfba768c7462b545be87a9b6e759b07085a777a
anasken <anashalim01@gmail.com>
2021-10-04 11:53:16 +0800

    arahan-arahan asas

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan 03</title>
 </head>
+
 <body>
     <h1>Asas PHP</h1>
-    <?php echo '<h2>hello</2>'; ?>
+
+    <?php echo '<h2>hello</2>'; 
+    $nama = 'gay';
+    $hadir = false;
+    $umur = 20;
+    $nokp = '010324-05-0119';
+    ?> 
 </body>
+
 </html>
